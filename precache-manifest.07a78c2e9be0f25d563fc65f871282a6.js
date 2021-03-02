ad2e9aa466c0ec4bbf03c6392c5159d525774d3c
Magda <jaskowska.magda@gmail.com>
2021-03-02 11:47:02 +0000

    Updates

---
+++ precache-manifest.07a78c2e9be0f25d563fc65f871282a6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfb9f9c9c183ac3ca38d",
+    "url": "/shop/static/css/main.b1b50d4e.chunk.css"
+  },
+  {
+    "revision": "cfb9f9c9c183ac3ca38d",
+    "url": "/shop/static/js/main.205ad23f.chunk.js"
+  },
+  {
+    "revision": "162d8e4e66ca27cdb508",
+    "url": "/shop/static/js/runtime~main.265007fa.js"
+  },
+  {
+    "revision": "c17d91f89e19ff008d83",
+    "url": "/shop/static/js/2.0cb06e6e.chunk.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/shop/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "3b2678c02d90aaa0d2ae380a119c234b",
+    "url": "/shop/static/media/times-circle-regular.3b2678c0.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/shop/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "a617dfb6304f7cd3ec2341bda2f99505",
+    "url": "/shop/static/media/user.a617dfb6.svg"
+  },
+  {
+    "revision": "d9d4427be3712f84989f4fbd31a58161",
+    "url": "/shop/static/media/long-arrow-alt-left-solid.d9d4427b.svg"
+  },
+  {
+    "revision": "82c810ba62f53f21f34c1f5cd01d3473",
+    "url": "/shop/static/media/hero.82c810ba.jpg"
+  },
+  {
+    "revision": "f2409fa8e248caec3d12978be16cd5e8",
+    "url": "/shop/index.html"
+  }
+];
